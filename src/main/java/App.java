63f30003233ae4e3539953b5561b839470b7006e
Mateusz Maciejewski <m.maciejewski93@gmail.com>
2022-04-09 17:54:52 +0200

    Added iteration of players list

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,6 +4,10 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
+    public static final int ROUNDS = 4;
+    public static final int MIN_PLAYERS = 2;
+    public static final int MAX_PLAYERS = 4;
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<Player> playersList = new ArrayList<>();
@@ -13,7 +17,7 @@ public class App {
         try {
             int numberOfPlayers = scanner.nextInt();
             scanner.nextLine();
-            if (numberOfPlayers >= 2 && numberOfPlayers <= 4) {
+            if (numberOfPlayers >= MIN_PLAYERS && numberOfPlayers <= MAX_PLAYERS) {
                 for (int i = 0; i < numberOfPlayers; i++) {
                     System.out.println("Proszę podać imie gracza nr " + (i + 1));
                     String playerName = scanner.nextLine();
@@ -26,5 +30,9 @@ public class App {
             System.out.println("Nie podales liczby calkowitej");
         }
         scanner.close();
+        for (int i = 0; i < ROUNDS; i++) {
+            System.out.println("Rozpoczęła się runda " + "<" + (i + 1) + ">");
+            playersList.forEach((player) -> System.out.println("Tura gracza: " + player.toString()));
+        }
     }
 }
